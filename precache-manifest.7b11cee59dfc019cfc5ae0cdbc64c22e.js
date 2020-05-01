b2db63a4109c892d86015fa74e83c87a86d649c7
Sdrummolo <52413900+Sdrummolo@users.noreply.github.com>
2020-05-01 22:15:33 +0100

    Updates

---
+++ precache-manifest.7b11cee59dfc019cfc5ae0cdbc64c22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606fc18d65271e97b71acc21369f9e28",
+    "url": "/tweeetter/index.html"
+  },
+  {
+    "revision": "5a9841348a754acd3f63",
+    "url": "/tweeetter/static/css/main.aba16752.chunk.css"
+  },
+  {
+    "revision": "efcb49e3e614ee919c28",
+    "url": "/tweeetter/static/js/2.df86f3b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tweeetter/static/js/2.df86f3b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9841348a754acd3f63",
+    "url": "/tweeetter/static/js/main.54295a22.chunk.js"
+  },
+  {
+    "revision": "f9a1def347037361ed1e",
+    "url": "/tweeetter/static/js/runtime-main.62f2eded.js"
+  }
+]);
